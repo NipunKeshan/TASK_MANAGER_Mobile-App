68a13bcf1ff16a14ef881fa627ae18dba7f2542e
NipunKeshan <nipunbis2002@gmail.com>
2024-05-10 20:33:32 +0530

    Create & Read Added

--- app/src/main/java/com/example/task_manager/MainActivity.kt
+++ app/src/main/java/com/example/task_manager/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.example.task_manager
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.task_manager.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var db:NotesDatabaseHelper
+    private lateinit var notesAdapter: NotesAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        db= NotesDatabaseHelper(this)
+        notesAdapter= NotesAdapter(db.getAllNotes(),this)
+
+        binding.notesRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.notesRecyclerView.adapter = notesAdapter
+
+
+        binding.addButton.setOnClickListener{
+            val intent = Intent(this,AddNoteActivity::class.java)
+            startActivity(intent)
+
+
+        }
     }
+
+    override fun onResume() {
+        super.onResume()
+        notesAdapter.refreshData(db.getAllNotes())
+    }
+
 }
---
+++ app/src/main/java/com/example/task_manager/Note.kt
@@ -0,0 +1,5 @@
+package com.example.task_manager
+
+
+
+data class Note(val id:Int, val title:String,val content:String)
