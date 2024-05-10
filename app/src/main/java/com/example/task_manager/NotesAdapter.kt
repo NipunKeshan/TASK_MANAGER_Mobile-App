2fad7cee364bd47201130658f54c4ef2a99032c4
NipunKeshan <nipunbis2002@gmail.com>
2024-05-10 22:45:02 +0530

    Delete Added

--- app/src/main/java/com/example/task_manager/NotesAdapter.kt
+++ app/src/main/java/com/example/task_manager/NotesAdapter.kt
@@ -7,14 +7,20 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
 class NotesAdapter(private var notes: List<Note>, private val context: Context) : RecyclerView.Adapter<NotesAdapter.NoteViewHolder>() {
+
+
+    private val db:NotesDatabaseHelper = NotesDatabaseHelper(context)
 
     class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val titleTextView: TextView = itemView.findViewById(R.id.titleTextView)
         val contentTextView: TextView = itemView.findViewById(R.id.contentTextView)
         val updateButton: ImageView = itemView.findViewById(R.id.updateButton)
+        val deleteButton: ImageView = itemView.findViewById(R.id.deleteButton)
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
@@ -34,6 +40,12 @@ class NotesAdapter(private var notes: List<Note>, private val context: Context) 
                 putExtra("note_id",note.id)
             }
             holder.itemView.context.startActivity(intent)
+        }
+        holder.deleteButton.setOnClickListener{
+            db.deleteNote(note.id)
+            refreshData(db.getAllNotes())
+            Toast.makeText(holder.itemView.context,"Note Delected",Toast.LENGTH_SHORT).show()
+
         }
     }
 
--- app/src/main/java/com/example/task_manager/NotesDatabaseHelper.kt
+++ app/src/main/java/com/example/task_manager/NotesDatabaseHelper.kt
@@ -86,4 +86,12 @@ class NotesDatabaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE
         return Note(id,title,content)
 
     }
+
+    fun deleteNote(noteId: Int){
+        val db=writableDatabase
+        val whereClause = "$COLUMN_ID =?"
+        val whereArgs = arrayOf(noteId.toString())
+        db.delete(TABLE_NAME,whereClause,whereArgs)
+        db.close()
+    }
 }
