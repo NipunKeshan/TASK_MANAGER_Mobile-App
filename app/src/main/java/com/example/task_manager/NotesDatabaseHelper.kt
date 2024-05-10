21acf128f1d2610d58745a534955bbfcd0c7801e
NipunKeshan <nipunbis2002@gmail.com>
2024-05-10 21:53:04 +0530

    Update Added

--- app/src/main/java/com/example/task_manager/NotesDatabaseHelper.kt
+++ app/src/main/java/com/example/task_manager/NotesDatabaseHelper.kt
@@ -1,5 +1,6 @@
 package com.example.task_manager
 
+import android.adservices.adid.AdId
 import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
@@ -55,5 +56,34 @@ class NotesDatabaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE
         cursor.close()
         db.close()
         return notesList
+    }
+
+    fun updateNote(note: Note){
+        val db = writableDatabase
+        val values = ContentValues().apply {
+            put(COLUMN_TITLE,note.title)
+            put(COLUMN_CONTENT,note.content)
+        }
+
+        val whereClause = "$COLUMN_ID=?"
+        val whereArgs = arrayOf(note.id.toString())
+        db.update(TABLE_NAME,values,whereClause,whereArgs)
+        db.close()
+
+    }
+    fun getNoteByID(noteId: Int):Note{
+        val db = readableDatabase
+        val query = "SELECT * FROM $TABLE_NAME WHERE $COLUMN_ID=$noteId"
+        val cursor = db.rawQuery(query,null)
+        cursor.moveToFirst()
+
+        val id=cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_ID))
+        val title=cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_TITLE))
+        val content=cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_CONTENT))
+
+        cursor.close()
+        db.close()
+        return Note(id,title,content)
+
     }
 }
