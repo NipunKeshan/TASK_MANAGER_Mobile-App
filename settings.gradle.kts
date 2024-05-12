5a442ac0e922f809b0a917e339ee42234b8c9f91
NipunKeshan <nipunbis2002@gmail.com>
2024-05-12 13:36:01 +0530

    Modified code error fixing

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "TASK_MANAGER"
+include(":app")
+ 
